707d429bb295d2c4ef88469e236d88ec468d147a
= <=>
2021-08-04 18:32:30 +0200

    submit tasks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ create a connection with redis database
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Callable, Optional, Union
 
 
 class Cache:
@@ -22,3 +22,20 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ get the element and return the decoded version"""
+        data = self._redis.get(key)
+        if (fn is not None):
+            return fn(data)
+        return data
+
+    def get_str(self, data: str) -> str:
+        """ return the decoded byte in string """
+        return data.decode('utf-8')
+
+    def get_int(self, data: str) -> int:
+        """ return the decoded byte in integer """
+        return int(data)
